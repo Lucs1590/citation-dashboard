89024fd872d0b6fa525d45537462e1a4d68b5e19
Lucas Brito <lucasbsilva29@gmail.com>
2021-05-01 19:24:39 -0300

    feat(app.component): add form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'citation-dashboard';
+export class AppComponent implements OnInit {
+  form: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+  ) { }
+
+  ngOnInit(): void {
+    this.createForm();
+  }
+
+  createForm(): void {
+    this.form = this.formBuilder.group({
+      tag: ['', Validators.required],
+      author: [''],
+      startYear: [''],
+      endYear: [''],
+      public: true
+    })
+  }
+
+  submit() {
+    // aqui você pode implementar a logica para fazer seu formulário salvar
+    console.log(this.form.value);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule,  
+    ReactiveFormsModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
       // Register the ServiceWorker as soon as the app is stable
