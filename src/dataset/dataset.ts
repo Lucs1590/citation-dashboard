c10a787ee051320e5225f9187d9ebda462e518f2
Lucas Brito <lucasbsilva29@gmail.com>
2021-07-23 11:37:35 -0300

    feat: add more datasets

--- src/dataset/dataset.ts
+++ src/dataset/dataset.ts
@@ -1,6 +1,21 @@
+import { br2018 } from "./br2018";
+import { jp2015 } from "./jp2015";
+import { mn2013 } from "./mn2013";
+import { mn2014 } from "./mn2014";
 import { vv2009 } from "./vv2009";
+import { vv2010 } from "./vv2010";
+import { vv2011 } from "./vv2011";
+import { vv2013 } from "./vv2013";
+import { vv2014 } from "./vv2014";
 
 export const dataset = [
     vv2009,
-
+    vv2010,
+    vv2011,
+    vv2013,
+    vv2014,
+    mn2013,
+    mn2014,
+    jp2015,
+    br2018
 ]
