f4edea86f72a5935e14eb2c5f64621b8a8df2aeb
Lucas Brito <lucasbsilva29@gmail.com>
2021-07-23 13:20:17 -0300

    feat: adding missing datasets

--- src/dataset/dataset.ts
+++ src/dataset/dataset.ts
@@ -1,7 +1,10 @@
 import { br2018 } from "./br2018";
+import { br2019 } from "./br2019";
 import { jp2015 } from "./jp2015";
 import { mn2013 } from "./mn2013";
 import { mn2014 } from "./mn2014";
+import { st2020 } from "./st2020";
+import { sv2018 } from "./sv2018";
 import { vv2009 } from "./vv2009";
 import { vv2010 } from "./vv2010";
 import { vv2011 } from "./vv2011";
@@ -17,5 +20,8 @@ export const dataset = [
     mn2013,
     mn2014,
     jp2015,
-    br2018
+    br2018,
+    br2019,
+    sv2018,
+    st2020
 ]
