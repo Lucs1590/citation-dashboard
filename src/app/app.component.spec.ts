d467b16bf74e92ffa10caaa022a5e005028c917f
Lucas Brito <lucasbsilva29@gmail.com>
2021-07-23 00:33:32 -0300

    refactor: changing project name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'citation-dashboard'`, () => {
+  it(`should have as title 'triathlon-dashboard'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('citation-dashboard');
+    expect(app.title).toEqual('triathlon-dashboard');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('citation-dashboard app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('triathlon-dashboard app is running!');
   });
 });
