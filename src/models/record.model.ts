20a8eeafcefd89c46412f5929730f828e60c9235
Lucas Brito <lucasbsilva29@gmail.com>
2021-07-24 13:45:12 -0300

    feat(record.model.t): transform time to second

--- src/models/record.model.ts
+++ src/models/record.model.ts
@@ -6,12 +6,12 @@ export class Record implements Deserializable {
   public country?: string;
   public startPosition?: number;
   public endPosition?: string;
-  public swim?: string;
-  public t1?: string;
-  public bike?: string;
-  public t2?: string;
-  public run?: string;
-  public totalTime?: string;
+  public swim?: number;
+  public t1?: number;
+  public bike?: number;
+  public t2?: number;
+  public run?: number;
+  public totalTime?: number;
   public program?: string;
 
   deserialize(input: any): this {
@@ -21,12 +21,20 @@ export class Record implements Deserializable {
     this.country = input['NATIONALITY'];
     this.startPosition = input['START NUMBER'];
     this.endPosition = input['POSITION'];
-    this.swim = input['SWIM'];
-    this.t1 = input['T1'];
-    this.bike = input['T2'];
-    this.t2 = input['T2'];
-    this.run = input['RUN'];
-    this.totalTime = input['TOTAL TIME'];
+    this.swim = toTimeSecond(input['SWIM']);
+    this.t1 = toTimeSecond(input['T1']);
+    this.bike = toTimeSecond(input['T2']);
+    this.t2 = toTimeSecond(input['T2']);
+    this.run = toTimeSecond(input['RUN']);
+    this.totalTime = toTimeSecond(input['TOTAL TIME']);
     return this;
   }
+}
+
+function toTimeSecond(time: string): number {
+  const newTime = String(time).split(':')
+  const hour = +newTime[0] * 3600;
+  const minute = +newTime[1] * 60;
+  const second = +newTime[2];
+  return hour + minute + second;
 }
