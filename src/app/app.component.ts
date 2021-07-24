0d164e12f754024ffe1a0bbdf698fbeb8f7473d7
Lucas Brito <lucasbsilva29@gmail.com>
2021-07-24 13:00:45 -0300

    feat: add countries dataset

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 import * as bootstrap from "bootstrap";
 import { dataset } from 'src/dataset/dataset';
+import { contries } from 'src/dataset/countries';
 import { Proof } from 'src/models/proof.model';
 import { Record } from 'src/models/record.model';
 
@@ -13,6 +14,7 @@ import { Record } from 'src/models/record.model';
 export class AppComponent implements OnInit, AfterViewInit {
   form: FormGroup;
   _dataset: Proof;
+  _countries: { name: string; code: string; }[];
   currentDataset: { name?: string; dataset?: Record[]; };
 
   constructor(
@@ -28,6 +30,7 @@ export class AppComponent implements OnInit, AfterViewInit {
     this.createForm();
     this._dataset = this.createDataset();
     this.currentDataset = this._dataset[0];
+    this._countries = contries;
   }
 
   ngAfterViewInit(): void {
